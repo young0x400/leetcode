6e4589c5bbaf7c627f5b36a753db87117b03e7fd
Young <80713349@qq.com>
2015-07-22 08:54:32 +0800

    word search first

---
+++ Word_Search.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<fstream>
+#include<ctime>
+#include<bitset>
+#include<algorithm>
+using namespace std;
+
+int main()
+{	
+	while(1)
+	{
+		int a;
+		cin>>a;
+		cout<<a%5<<endl;
+	}
+	return 0;
+}
